3f4d36cca77e576e2e23a7e2e67d5c3761d817b5
Matheus Gabriel Werny de Lima <matheusgwdl@protonmail.com>
2019-10-13 10:21:33 +0200

    making more objects instead of namespaces

---
+++ src/SpecialTools.cpp
@@ -0,0 +1,20 @@
+#include "tgbot/SpecialTools.h"
+#include <string>
+#include <rapidjson/document.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
+#include <tgbot/http/HttpArg.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+namespace tgbot
+{
+	std::string SpecialTools::get_json_obj_as_string(const rapidjson::Value &val)
+	{
+		rapidjson::StringBuffer sb;
+		rapidjson::Writer<rapidjson::StringBuffer> writer(sb);
+		val.Accept(writer);
+		return sb.GetString();
+	}
+}
