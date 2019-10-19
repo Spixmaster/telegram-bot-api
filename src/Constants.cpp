ff9d750b0713efb4a1993bc208dc94968702bc9a
Matheus Gabriel Werny de Lima <spixmaster528@gmail.com>
2019-10-19 08:09:38 +0200

    updated version

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -5,7 +5,7 @@
 namespace tgbot
 {
 	std::string Constants::version = "1.0.0";
-	std::string Constants::release = "11.Oct.2019";
+	std::string Constants::release = "18.Oct.2019";
 
 	void Constants::file_non_existent(const std::string &file)
 	{
