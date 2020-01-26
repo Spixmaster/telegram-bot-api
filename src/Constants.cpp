fe4d5d4430bf4a1c9656bba7f58941399c7672c0
Spixmaster <spixmaster528@gmail.com>
2020-01-26 22:51:20 +0100

    fixed header

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -1,9 +1,8 @@
 #include "tgbot/Constants.h"
 #include <string>
-#include <iostream>
 
 namespace tgbot
 {
-	const std::string Constants::version = "1.1.0";
+	const std::string Constants::version = "1.1.1";
 	const std::string Constants::release = "26.Jan.2020";
 }
