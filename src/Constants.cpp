4288b6ca2379d86dc43e0291df3021899742351e
Spixmaster <spixmaster528@gmail.com>
2020-02-02 14:16:15 +0100

    removed todos

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -4,5 +4,5 @@
 namespace tgbot
 {
 	const std::string Constants::version = "1.1.5";
-	const std::string Constants::release = "27.Jan.2020";
+	const std::string Constants::release = "02.Feb.2020";
 }
