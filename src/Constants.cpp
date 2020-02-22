61384c622f5277ed881bba66d4118dbb95273a1f
Matheus Gabriel Werny de Lima <matheusgwdl@protonmail.com>
2020-02-22 15:25:11 +0100

    updated version

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -4,5 +4,5 @@
 namespace tgbot
 {
 	const std::string Constants::version = "1.1.8";
-	const std::string Constants::release = "16.Feb.2020";
+	const std::string Constants::release = "22.Feb.2020";
 }
