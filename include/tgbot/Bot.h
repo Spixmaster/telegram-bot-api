3f478029fa9e992898fab71e92cb9006e736bb40
Spixmaster <spixmaster528@gmail.com>
2020-05-22 11:24:24 +0200

    Submodule updated.

--- include/tgbot/Bot.h
+++ include/tgbot/Bot.h
@@ -11,6 +11,9 @@
  * @author Matheus Gabriel Werny de Lima
  * @copyright GNU General Public License v2.0
  * @version
+ * 1.2.8 (22.05.2020)
+ * - Submodule updated.
+ * @version
  * 1.2.7 (15.05.2020)
  * - Bug fixes.
  * @version
