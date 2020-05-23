d8f4889211758d2f9571ee4213fcdf2da512858e
Matheus Gabriel Werny de Lima <matheusgwdl@protonmail.com>
2020-05-23 21:00:18 +0200

    Submodule and contributing update.

--- src/constants/Constants.cpp
+++ src/constants/Constants.cpp
@@ -3,6 +3,6 @@
 
 namespace tgbot
 {
-	const std::string Constants::version = "1.2.8";
-	const std::string Constants::release = "22.May.2020";
+	const std::string Constants::version = "1.2.9";
+	const std::string Constants::release = "23.May.2020";
 }
