39b9d70796e94a2895c2bb113903cd27d47ddadd
Spixmaster <spixmaster528@gmail.com>
2020-05-25 14:35:55 +0200

    Change error output and improved messages.

--- src/constants/Messages.cpp
+++ src/constants/Messages.cpp
@@ -9,66 +9,66 @@ namespace tgbot
 
 	std::string Messages::field_does_not_contain_string(const std::string &field) noexcept
 	{
-		return boost::str(boost::format("The field %1% does not contain a string.") % field);
+		return boost::str(boost::format("The field \"%1%\" does not contain a string.") % field);
 	}
 
 	std::string Messages::field_does_not_contain_int(const std::string &field) noexcept
 	{
-		return boost::str(boost::format("The field %1% does not contain an integer.") % field);
+		return boost::str(boost::format("The field \"%1%\" does not contain an integer.") % field);
 	}
 
 	std::string Messages::field_does_not_contain_int64(const std::string &field) noexcept
 	{
-		return boost::str(boost::format("The field %1% does not contain a 64 bits integer.") % field);
+		return boost::str(boost::format("The field \"%1%\" does not contain a 64 bits integer.") % field);
 	}
 
 	std::string Messages::field_does_not_contain_json_obj(const std::string &field) noexcept
 	{
-		return boost::str(boost::format("The field %1% does not contain a JSON object.") % field);
+		return boost::str(boost::format("The field \"%1%\" does not contain a JSON object.") % field);
 	}
 
 	std::string Messages::field_does_not_contain_json_arr(const std::string &field) noexcept
 	{
-		return boost::str(boost::format("The field %1% does not contain a JSON array.") % field);
+		return boost::str(boost::format("The field \"%1%\" does not contain a JSON array.") % field);
 	}
 
 	std::string Messages::field_element_does_not_contain_json_arr(const std::string &field) noexcept
 	{
-		return boost::str(boost::format("The element of field %1% does not contain a JSON array.") % field);
+		return boost::str(boost::format("The element of field \"%1%\" does not contain a JSON array.") % field);
 	}
 
 	std::string Messages::field_element_does_not_contain_json_obj(const std::string &field) noexcept
 	{
-		return boost::str(boost::format("The element of field %1% does not contain a JSON object.") % field);
+		return boost::str(boost::format("The element of field \"%1%\" does not contain a JSON object.") % field);
 	}
 
 	std::string Messages::field_non_existent(const std::string &field) noexcept
 	{
-		return boost::str(boost::format("There is not field %1% in the JSON object.") % field);
+		return boost::str(boost::format("There is not field \"%1%\" in the JSON object.") % field);
 	}
 
 	std::string Messages::field_does_not_contain_bool(const std::string &field) noexcept
 	{
-		return boost::str(boost::format("The field %1% does not contain a boolean.") % field);
+		return boost::str(boost::format("The field \"%1%\" does not contain a boolean.") % field);
 	}
 
 	std::string Messages::field_does_not_contain_float(const std::string &field) noexcept
 	{
-		return boost::str(boost::format("The field %1% does not contain a float.") % field);
+		return boost::str(boost::format("The field \"%1%\" does not contain a float.") % field);
 	}
 
 	std::string Messages::field_element_element_does_not_contain_json_obj(const std::string &field) noexcept
 	{
-		return boost::str(boost::format("The element's element of field %1% does not contain a JSON object.") % field);
+		return boost::str(boost::format("The element's element of field \"%1%\" does not contain a JSON object.") % field);
 	}
 
 	std::string Messages::field_element_does_not_contain_string(const std::string &field) noexcept
 	{
-		return boost::str(boost::format("The element of field %1% does not contain a string.") % field);
+		return boost::str(boost::format("The element of field \"%1%\" does not contain a string.") % field);
 	}
 
 	std::string Messages::field_element_does_not_contain_int(const std::string &field) noexcept
 	{
-		return boost::str(boost::format("The element of field %1% does not contain an integer.") % field);
+		return boost::str(boost::format("The element of field \"%1%\" does not contain an integer.") % field);
 	}
 }
