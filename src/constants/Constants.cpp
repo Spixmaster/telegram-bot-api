ec5b4091b5276db4a8d67da6616c6eded586c0df
Spixmaster <spixmaster528@gmail.com>
2020-06-19 16:07:27 +0200

    Submodule updated.

--- src/constants/Constants.cpp
+++ src/constants/Constants.cpp
@@ -3,6 +3,6 @@
 
 namespace tgbot
 {
-	const std::string Constants::version = "1.3.5";
-	const std::string Constants::release = "18.Jun.2020";
+	const std::string Constants::version = "1.3.6";
+	const std::string Constants::release = "19.Jun.2020";
 }
