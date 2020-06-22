0a613ecf4c8638ca84d83489fd9d42402c723284
Matheus Gabriel Werny de Lima <matheusgwdl@protonmail.com>
2020-06-22 19:51:38 +0200

    Submodule update.

--- include/tgbot/Bot.h
+++ include/tgbot/Bot.h
@@ -11,6 +11,9 @@
  * @author Matheus Gabriel Werny de Lima
  * @copyright GNU General Public License v2.0
  * @version
+ * 1.3.12 (22.06.2020)
+ * - Submodule update.
+ * @version
  * 1.3.11 (22.06.2020)
  * - Submodule update.
  * @version
